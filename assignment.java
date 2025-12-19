7a4625ed27888ce29c4f0cc59ee5791e96918fb7
kajal <105410435+kajal53@users.noreply.github.com>
2025-12-19 10:16:37 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,14 @@
+public class assignment {
+
+    public static void main(String[] args) {
+
+        // Number Triangle (Repeated Numbers)
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
